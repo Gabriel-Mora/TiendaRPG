3a255ba838a9db98daf7a7c46b47ab408c12f427
Gabriel-Mora <71365238+Gabriel-Mora@users.noreply.github.com>
2020-10-13 18:35:19 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+
+/**
+ *
+ * @author gmora
+ */
+public class Main {
+    //atributos
+    int Vida =200;
+    int Ataque =50;
+    int Sigilo=20;
+    int Armadura=0;
+    int Escudo=0;
+    int Vida2 =200;
+    int Ataque2 =50;
+    int Sigilo2=20;
+    int Armadura2=0;
+    int Escudo2=0;
+    
+    public static void main(String[] args){      
+        
+        }      
+ }
+   
